c8d7b1cf3602e7101bbba11d0973e2b855e88717
christianpepscaasi-gh <christianpepscaasi@gmail.com>
2026-01-12 23:37:08 +0800

    Method Java

---
+++ Basics/Method.java
@@ -0,0 +1,13 @@
+public class Method {
+
+    // method = a block of reusable code that is executed when called.
+
+    public static void main(String[] args) {
+        HappyBirthday();
+
+    }
+
+    static void HappyBirthday() {
+        System.out.println("Happy Birthday to you!");
+    }
+}
